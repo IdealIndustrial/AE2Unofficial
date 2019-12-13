60eb62aaad11deea9a65fcf56e3b0c437c6894e3
wvk17 <wvk17@yandex.ru>
2019-12-14 02:01:19 +0300

    Moved the cache up a level, so that pattern terminal also would use it.

--- src/main/java/appeng/container/slot/SlotCraftingTerm.java
+++ src/main/java/appeng/container/slot/SlotCraftingTerm.java
@@ -56,7 +56,6 @@ public class SlotCraftingTerm extends AppEngCraftingSlot
 	private final IEnergySource energySrc;
 	private final IStorageMonitorable storage;
 	private final IContainerCraftingPacket container;
-	private static IRecipe lastUsedRecipe = null;
 
 	public SlotCraftingTerm( final EntityPlayer player, final BaseActionSource mySrc, final IEnergySource energySrc, final IStorageMonitorable storage, final IInventory cMatrix, final IInventory secondMatrix, final IInventory output, final int x, final int y, final IContainerCraftingPacket ccp )
 	{
@@ -171,9 +170,7 @@ public class SlotCraftingTerm extends AppEngCraftingSlot
 					ic.setInventorySlotContents( x, this.getPattern().getStackInSlot( x ) );
 				}
 
-				if (lastUsedRecipe == null || !lastUsedRecipe.matches(ic, p.worldObj))
-					lastUsedRecipe = Platform.findMatchingRecipe(ic, p.worldObj);
-				final IRecipe r = lastUsedRecipe;
+				final IRecipe r = Platform.findMatchingRecipe(ic, p.worldObj);
 
 				if( r == null )
 				{
